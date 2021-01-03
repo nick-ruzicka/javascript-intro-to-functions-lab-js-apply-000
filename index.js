37b492be9ca5afe2be3e432cefd9277257f831fa
Nicholas Ruzicka <nick.c.ruzicka@gmail.com>
2021-01-03 04:00:37 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+return string.toUpperCase()
+}
+"learn test";
+"learn test";
+function whisper(string) {
+  return string.toLowerCase()
+}
+"learn test";
+function logShout(string) {
+ console.log(string.toUpperCase)
+}
+function logWhisper(string) {
+  return string.toUpperCase()
+}
+function sayHitoGrandma(string) {
+  return string.
